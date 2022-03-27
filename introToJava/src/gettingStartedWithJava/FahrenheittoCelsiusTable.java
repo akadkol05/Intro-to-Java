856ae688e956d17b44de7080664acf9d7f106c8b
akadkol05 <akadkol05@gmail.com>
2022-03-27 21:07:47 +0530

    Fahrenheit to celsius

---
+++ introToJava/src/gettingStartedWithJava/FahrenheittoCelsiusTable.java
@@ -0,0 +1,16 @@
+package gettingStartedWithJava;
+
+import java.util.Scanner;
+
+public class FahrenheittoCelsiusTable {
+
+	public static void main(String[] args) {
+		Scanner sc =  new Scanner(System.in);
+		int s = sc.nextInt();
+		int e = sc.nextInt();
+		int w = sc.nextInt();
+		
+
+	}
+
+}
