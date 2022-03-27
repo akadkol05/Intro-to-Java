8cc391d66b938674480ff4becf28cf95093d9928
akadkol05 <akadkol05@gmail.com>
2022-03-27 21:08:53 +0530

    Power of x

---
+++ introToJava/src/gettingStartedWithJava/PowerOfX.java
@@ -0,0 +1,16 @@
+package gettingStartedWithJava;
+
+public class PowerOfX {
+
+	public static void main(String[] args) {
+		int x=2;
+		int n=5;
+		int power=1;
+		for(int i=0;i<n;i++) {
+			 power=power*x;
+		}
+		System.out.println(power);
+
+	}
+
+}
