96c5388a6bfd11aff799f708fb1879bc8da25e51
akadkol05 <akadkol05@gmail.com>
2022-03-27 21:08:31 +0530

    Multiplication table

---
+++ introToJava/src/gettingStartedWithJava/MultiplicationTable.java
@@ -0,0 +1,16 @@
+package gettingStartedWithJava;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		int n=2;
+		int mul=1;
+		 for(int i=0;i<=10;i++) {
+			  mul=n*i;
+			  System.out.println("2x"+i+"="+ mul);
+		 }
+		 
+
+	}
+
+}
