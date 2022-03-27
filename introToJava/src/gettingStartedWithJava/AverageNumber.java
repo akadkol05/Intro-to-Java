9bce3474cc54d14d0afa0f7eeb22a62763c52208
akadkol05 <akadkol05@gmail.com>
2022-03-27 21:06:06 +0530

    average number

---
+++ introToJava/src/gettingStartedWithJava/AverageNumber.java
@@ -0,0 +1,12 @@
+package gettingStartedWithJava;
+
+public class AverageNumber {
+
+	public static void main(String[] args) {
+	 int a=2 , b=3,c=4,d=5;
+	 int avg=(a+b+c+d)/4;
+	 System.out.println(avg);
+
+	}
+
+}
