296e29458de418121463ca9ef6a79ce53a0a323c
akadkol05 <akadkol05@gmail.com>
2022-03-29 23:12:33 +0530

    Reverse a number

---
+++ introToJava/src/gettingStartedWithJava/ReverseNumber.java
@@ -0,0 +1,18 @@
+package gettingStartedWithJava;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		int n=1234;
+		int rev=0;
+		while(n!=0) {
+			int num=n%10;
+			rev=rev*10+num;
+			n/=10;
+		}
+		
+		System.out.println(rev);
+
+	}
+
+}
