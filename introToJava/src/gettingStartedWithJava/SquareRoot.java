848b49767b332e2e43dd7d1f3a120d3fd572a86c
akadkol05 <akadkol05@gmail.com>
2022-03-29 23:13:03 +0530

    square root

---
+++ introToJava/src/gettingStartedWithJava/SquareRoot.java
@@ -0,0 +1,27 @@
+package gettingStartedWithJava;
+
+public class SquareRoot {
+
+	public static void main(String[] args) {
+	   int n=10;
+	   int sqr=1;
+	   for(int i=0;i*i<=n;i++) {
+//		   sqr=i*i;
+//		   if(sqr==n) {
+//			   System.out.println(i);
+//			   return;
+//		   }
+//		   if(sqr>n) {
+//			   System.out.println(i-1);
+//			   return;
+//		   }
+		   
+		   sqr=i;
+		   
+		   
+	   }
+	   System.out.println(sqr);
+
+	}
+
+}
