89c664224b1857ac1a1664ea0201bd6ebf254287
akadkol05 <akadkol05@gmail.com>
2022-04-01 23:40:44 +0530

    rotate array

---
+++ introToJava/src/arrays2/RotateArray.java
@@ -0,0 +1,29 @@
+package arrays2;
+
+public class RotateArray {
+
+	public static void main(String[] args) {
+		int arr[]= {1,2,3,4,5,6,7,8,9,10};
+		int d=5;
+		
+		arr=rotate(arr,d);
+		
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+
+	}
+	public static int[] rotate(int arr[],int d) {
+		int a[]=new int[d];
+		for(int i=0;i<a.length;i++) {
+			a[i]=arr[i];
+		}
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=arr[i+d];
+		}
+		for(int i=0;)
+		return arr;
+		
+	}
+
+}
